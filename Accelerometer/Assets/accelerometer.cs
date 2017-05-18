c6c88b86986a623090aafa4ee7a9015e651612d8
lgoncalves <lgoncalves@globalshares.com>
2017-05-18 18:26:46 +0100

    first commit

---
+++ Accelerometer/Assets/accelerometer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class accelerometer : MonoBehaviour {
+
+    private Rigidbody rigid;
+
+	// Use this for initialization
+	private void Start ()
+    {
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        rigid.AddForce(Input.acceleration);
+	}
+}
